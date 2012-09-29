1b104185526f1a41f53b5b3dd298d9efb4a272c7
Paul Serby <paul.serby@clock.co.uk>
2012-09-29 04:43:57 +0100

    Better error handling

--- proxy.js
+++ proxy.js
@@ -1,7 +1,19 @@
 var http = require('http')
-  , httpProxy = require('http-proxy');
+  , httpProxy = require('http-proxy')
+  , request = require('request')
+  ;
 
 var proxy = new httpProxy.RoutingProxy();
+
+proxy.on('proxyError', function(error, req, res) {
+  request.get('http://localhost:3002' + req.url).on('error', function() {
+    res.writeHead(500, {
+      'Content-Type': 'text/plain'
+    });
+
+    res.end('Something went wrong. And we are reporting a custom error message.');
+  }).pipe(res);
+});
 
 http.createServer(function (req, res) {
 
@@ -10,4 +22,4 @@ http.createServer(function (req, res) {
     port: 3001
   });
 
-}).listen(80);
+}).listen(process.env.PORT || 80);
