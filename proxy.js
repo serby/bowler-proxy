ae54ad361862b3040333dcd3ec69d68fd07aeddd
Paul Serby <paul.serby@clock.co.uk>
2012-09-29 04:20:04 +0100

    Back to http-proxy

--- proxy.js
+++ proxy.js
@@ -1,14 +1,13 @@
 var http = require('http')
-  , request = require('request')
-  , url = require('url')
-  , port = process.env.PORT || 3000 ;
+  , httpProxy = require('http-proxy');
+
+var proxy = new httpProxy.RoutingProxy();
 
 http.createServer(function (req, res) {
 
-  request.get('http://localhost:3001' + req.url).on('error', function(error) {
-    request.get('http://localhost:3002' + req.url).pipe(res);
-  }).pipe(res);
+  proxy.proxyRequest(req, res, {
+    host: 'localhost',
+    port: 3001
+  });
 
-}).listen(port);
-
-console.log('Starting on http://localhost:' + port);
+}).listen(8001);
